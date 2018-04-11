f87cad16bf0c80c6f3e965dedd1179cb5437c213
Ubuntu <ubuntu@ip-172-31-52-238.ec2.internal>
2018-04-11 19:41:59 +0000

    removed the keys

--- sitesearch-mw/start.sh
+++ sitesearch-mw/start.sh
@@ -4,6 +4,4 @@ then
 	exit 1
 fi
 cp config/config.$1.js config/config.js
-export SECRET_ACCESS_KEY=8qEuoc0Rmrl4lViZ22BJcfPzPX+Uvy6xdkj6kzjz
-export ACCESS_KEY_ID=AKIAJ3CBWMH67UXP4MMQ
 node index.js
