fcf31937fb3571878fd1331e2aabeccd6e66df74
Palani <vasudevan.palani@gmail.com>
2018-04-14 16:18:53 -0400

    Working version of notification

--- sitesearch-api/handler.js
+++ sitesearch-api/handler.js
@@ -11,11 +11,13 @@ module.exports.welcome = (event, context, callback) => {
 };
 
 module.exports.notification = (event, context, callback) => {
-  console.log(event,context);
+  console.log(event);
 
-  let id = event.params['query']['id'];
-  let status = event.params['query']['status'];
-  let token = event['query']['token']
+  let id = event.params['querystring']['id'];
+  let status = event.params['querystring']['status'];
+  let token = event.params['querystring']['token']
+
+  console.log(id,status,token);
 
   let websiteStatus = "FAILED";
   if(status == "KILLED"){
@@ -31,20 +33,32 @@ module.exports.notification = (event, context, callback) => {
     websiteStatus = 'COMPLETED';
   }
 
-  axios.get(config.firebase.url+"/crawl-Q/"+id+".json?auth="+token).then(job => {
+  axios.get(config.firebase.url+"/crawl-Q/"+id+".json?auth="+token).then(response => {
+    let job = response.data;
+    console.log(job);
     let websiteId = job.website.id;
-    axios.put(config.firebase.url+"/websites/"+websiteId+".json?auth="+token,{
-      'status' : websiteStatus
-    }).
-    catch(err => {
-      throw new Error("internalerror: Failed to update status : "+status+", job :"+id);
+    axios.get(config.firebase.url+"/websites.json?orderBy=\"id\"&equalTo=\""+websiteId+"\"&auth="+token)
+    .then(resp => {
+      console.log(resp.data);
+      let websitekey = Object.keys(resp.data)[0];
+      console.log(websitekey);
+      axios.put(config.firebase.url+"/websites/"+websitekey+"/status.json?auth="+token,"\""+websiteStatus+"\"")
+      .then(updateresp => {
+        console.log(updateresp.data);
+        callback(null,updateresp.data);
+      })
+      .catch(updateerr => {
+        throw new Error("internalerror: Failed to update status ** : "+status+", job :"+id+", "+updateerr);
+      });
+    })
+    .catch(websitegeterr => {
+      throw new Error("internalerror: Failed to update status * : "+status+", job :"+id+", "+websitegeterr);
     });
   })
   .catch(err =>{
-    throw new Error("internalerror: Failed to update status : "+status+", job :"+id);
+    throw new Error("internalerror: Failed to update status : "+status+", job :"+id+", "+err);
   });
 
-  callback(null,{});
 }
 
 module.exports.crawl = (event, context, callback) => {
