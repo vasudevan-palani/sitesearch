28fd004102e712b25d501cb9baa074dfb8b8b0c9
Palani <vasudevan.palani@gmail.com>
2018-05-08 16:21:54 -0400

    Fixed prod build

--- sitesearch-web/src/app/contact-us/contact-us.component.ts
+++ sitesearch-web/src/app/contact-us/contact-us.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
+  public isSubmitInProgress: boolean;
+
   constructor() { }
 
   ngOnInit() {
