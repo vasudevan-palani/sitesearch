084f41927cab7265a735787342152df7647af0ea
Palani <vasudevan.palani@gmail.com>
2018-05-16 15:21:07 -0400

    Fixed facebook link

--- sitesearch-web/src/app/main/main.component.ts
+++ sitesearch-web/src/app/main/main.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: 'main.component.html',
-  styleUrls: ['main.component.scss']
+  styleUrls: ['main.component.scss'],
+  animations: [
+    trigger('fadeLeft', [
+      transition(':enter', [
+        style({opacity: 0,transform: 'translateX(-10%)'}),
+        animate('500ms ease-in')
+      ])
+    ]),
+    trigger('fadeRight', [
+      transition(':enter', [
+        style({opacity: 0,transform: 'translateX(+10%)'}),
+        animate('500ms ease-in')
+      ])
+    ])
+  ]
 })
 export class AppMainComponent {
   title = 'app works!';
