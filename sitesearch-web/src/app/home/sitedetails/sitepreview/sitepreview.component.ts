4758d90b94e8e8d83431f53392cafaa9970e53ba
Palani <vasudevan.palani@gmail.com>
2018-07-25 16:44:30 -0400

    Added auto complete

--- sitesearch-web/src/app/home/sitedetails/sitepreview/sitepreview.component.ts
+++ sitesearch-web/src/app/home/sitedetails/sitepreview/sitepreview.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SiteService } from 'app/services/site.service';
 import { ActivatedRoute } from '@angular/router';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/merge';
+
+const WIKI_URL = 'https://en.wikipedia.org/w/api.php';
+const PARAMS = new HttpParams({
+  fromObject: {
+    action: 'opensearch',
+    format: 'json',
+    origin: '*'
+  }
+});
+
 @Component({
   selector: 'app-sitepreview',
   templateUrl: './sitepreview.component.html',
@@ -8,7 +28,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SitePreviewComponent implements OnInit {
 
-  constructor(private siteSvc : SiteService,private route : ActivatedRoute) { }
+  searching = false;
+  searchFailed = false;
+  hideSearchingWhenUnsubscribed = new Observable(() => () => this.searching = false);
+
+  constructor(private siteSvc : SiteService,private route : ActivatedRoute, private http: HttpClient) { }
 
   public response: any;
   private siteid : string;
@@ -30,4 +54,26 @@ export class SitePreviewComponent implements OnInit {
     window["dataLayer"].push({'event':'test-event','data':data})
     this.siteSvc.search(data).subscribe(response => this.response = response);
   }
+
+  httpSearch(term:string){
+    return this.siteSvc.search({'tag':term,'siteid':this.siteid}).map(response => response.suggestions);
+    // return this.http
+    //   .get(WIKI_URL, {params: PARAMS.set('search', term)})
+    //   .map(response => response[1]);
+  }
+
+  autosearch = (text$: Observable<string>) =>
+  text$
+    .debounceTime(300)
+    .distinctUntilChanged()
+    .do(() => this.searching = true)
+    .switchMap(term =>
+      this.httpSearch(term)
+        .do(() => this.searchFailed = false)
+        .catch(() => {
+          this.searchFailed = true;
+          return of([]);
+        }))
+    .do(() => this.searching = false)
+    .merge(this.hideSearchingWhenUnsubscribed);
 }
